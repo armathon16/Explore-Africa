ab0bedb4c7894a6ebbce970a7da68c638630e50b
ARMATHON <mangwengwejr@gmail.com>
2026-06-13 13:59:06 +0200

    This is my last day of commiting to this project

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import random
+# -----------------------------
+# Global Variables
+# -----------------------------
+player_name = ""
+
+total_questions = 0
+total_correct = 0
+quizzes_played = 0
 
 # -----------------------------
 # Explore Africa
@@ -6,24 +14,64 @@ import random
 # By: AbdulRahman Masamba
 # -----------------------------
 
+def welcome_screen():
+    global player_name
+
+    print("\n" * 2)
+
+    print("=" * 80)
+
+    print(r"""
+ ███████╗██╗  ██╗██████╗ ██╗      ██████╗ ██████╗ ███████╗
+ ██╔════╝╚██╗██╔╝██╔══██╗██║     ██╔═══██╗██╔══██╗██╔════╝
+ █████╗   ╚███╔╝ ██████╔╝██║     ██║   ██║██████╔╝█████╗
+ ██╔══╝   ██╔██╗ ██╔═══╝ ██║     ██║   ██║██╔══██╗██╔══╝
+ ███████╗██╔╝ ██╗██║     ███████╗╚██████╔╝██║  ██║███████╗
+ ╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+                    🌍  EXPLORE AFRICA  🌍
+    """)
+
+    print("=" * 80)
+    print("      Discover • Learn • Explore • Challenge Yourself")
+    print()
+    print("🦁 Test your knowledge of African Capitals")
+    print("🐘 Discover Amazing Wildlife")
+    print("🏔️ Learn Geography & Fun Facts")
+    print()
+
+    player_name = input("👤 Enter your name, Explorer: ")
+
+    print()
+    print(f"🌍 Welcome, {player_name}!")
+    print("Get ready for an exciting journey across Africa!")
+    print()
+
+    input("Press Enter to begin your adventure...")
+
+    # Clear screen
+    for i in range(25):
+        print()
 
 def main():
     """
     Main function.
     Keeps showing the menu until the user decides to quit.
     """
+    welcome_screen()
 
     while True:
         print_banner()
 
-        print("1. Play Capitals Quiz")
-        print("2. Play Wildlife Quiz")
-        print("3. Play Fun Facts Quiz")
-        print("4. Instructions")
-        print("5. Exit")
-        print()
+        print("1. 🌍 African Capitals Quiz")
+        print("2. 🦁 Wildlife Quiz")
+        print("3. 📚 Africa Fun Facts")
+        print("4. 📊 Statistics Dashboard")
+        print("5. 🏆 Achievement Certificate")
+        print("6. 📖 Instructions")
+        print("7. 🚪 Exit")
 
-        choice = input("Choose an option (1-5): ")
+        choice = input("Choose an option (1-7): ")
 
         print()
 
@@ -37,10 +85,24 @@ def main():
             facts_quiz()
 
         elif choice == "4":
-            show_instructions()
-
+            show_statistics()
+        
         elif choice == "5":
-            print("Thank you for exploring Africa!")
+            show_certificate()
+        
+        elif choice== "6":
+            show_instructions()
+        
+
+        elif choice == "7":
+            print()
+            print("=" * 60)
+            print(f"🌍 Thank you for playing, {player_name}!")
+            print("We hope you enjoyed exploring Africa.")
+            print("Safe travels and keep learning!")
+            print()
+            print("Project created by AbdulRahman Masamba")
+            print("=" * 60)
             break
 
         else:
@@ -52,9 +114,13 @@ def main():
 # ------------------------------------------------
 def print_banner():
 
-    print("=" * 50)
-    print("        🌍 EXPLORE AFRICA 🌍")
-    print("=" * 50)
+    global player_name
+
+    print("=" * 60)
+    print("🌍 EXPLORE AFRICA: The Ultimate Quiz Adventure 🌍")
+    print("=" * 60)
+
+    print(f"Explorer: {player_name}")
     print("Learn • Play • Discover")
     print()
 
@@ -76,8 +142,114 @@ def show_instructions():
     input("Press Enter to return to the menu...")
     print()
 
+# ----------------------------
+# Show statistics dashboard
+# ----------------------------
 
+def show_statistics():
 
+    global player_name
+    global quizzes_played
+    global total_questions
+    global total_correct
+
+    print("=" * 60)
+    print("📊 PLAYER DASHBOARD")
+    print("=" * 60)
+
+    print(f"Explorer: {player_name}")
+    print(f"Quizzes Played: {quizzes_played}")
+    print(f"Questions Answered: {total_questions}")
+    print(f"Correct Answers: {total_correct}")
+
+    if total_questions > 0:
+        accuracy = (total_correct / total_questions) * 100
+    else:
+        accuracy = 0
+
+    print(f"Accuracy: {accuracy:.1f}%")
+
+    if accuracy >= 90:
+        print("🏆 Badge: Africa Legend")
+    elif accuracy >= 75:
+        print("🥇 Badge: Gold Explorer")
+    elif accuracy >= 50:
+        print("🥈 Badge: Silver Explorer")
+    else:
+        print("🥉 Badge: Rising Explorer")
+
+    print()
+
+    input("Press Enter to continue...")
+
+# ----------------------------------------------
+# This is a personlized certificate for the player
+# ------------------------------------------------
+def show_certificate():
+
+    global player_name
+    global total_questions
+    global total_correct
+    global quizzes_played
+
+    print("\n")
+
+    print("=" * 75)
+    print("🏆 CERTIFICATE OF ACHIEVEMENT 🏆")
+    print("=" * 75)
+    print()
+
+    print("            EXPLORE AFRICA")
+    print("        Code in Place Final Project")
+    print()
+
+    print(f"This certifies that")
+    print()
+
+    print(f"          {player_name.upper()}")
+    print()
+
+    print("has successfully completed the")
+    print("Explore Africa Learning Challenge.")
+    print()
+
+    print(f"Quizzes Completed : {quizzes_played}")
+    print(f"Questions Answered: {total_questions}")
+    print(f"Correct Answers   : {total_correct}")
+
+    if total_questions > 0:
+        accuracy = (total_correct / total_questions) * 100
+    else:
+        accuracy = 0
+
+    print(f"Overall Accuracy  : {accuracy:.1f}%")
+
+    print()
+
+    if accuracy == 100:
+        badge = "🏆 AFRICA LEGEND"
+
+    elif accuracy >= 90:
+        badge = "🥇 GOLD EXPLORER"
+
+    elif accuracy >= 75:
+        badge = "🥈 SILVER EXPLORER"
+
+    elif accuracy >= 50:
+        badge = "🥉 BRONZE EXPLORER"
+
+    else:
+        badge = "🌱 RISING EXPLORER"
+
+    print(f"Achievement Badge : {badge}")
+
+    print()
+    print("Congratulations on your learning journey!")
+    print()
+
+    print("=" * 75)
+
+    input("\nPress Enter to continue...")
 # Capitals Quiz
 # ------------------------------------------------
 def capitals_quiz():
@@ -86,7 +258,9 @@ def capitals_quiz():
     Questions are shuffled so the quiz
     is different each time.
     """
-
+    global total_questions
+    global total_correct
+    global quizzes_played
     # Dictionary:
     # Key = Country
     # Value = Capital
@@ -144,7 +318,7 @@ def capitals_quiz():
     print("Quiz Complete!")
     print("=" * 40)
 
-    print(f"You scored {score}/{len(capitals)}")
+    print(f"🎉 {player_name}, you scored {score}/{len(capitals)}!")
 
     percentage = (score / len(capitals)) * 100
 
@@ -164,7 +338,9 @@ def capitals_quiz():
         print("📚 Keep studying and try again!")
 
     print()
-
+    total_questions += len(capitals)
+    total_correct += score
+    quizzes_played += 1
     input("Press Enter to return to the main menu...")
 
 
@@ -178,7 +354,9 @@ def wildlife_quiz():
     """
     Tests the user's knowledge of African wildlife.
     """
-
+    global total_questions
+    global total_correct
+    global quizzes_played
     # Each question is stored as a tuple:
     # (question, correct_answer)
     questions = [
@@ -242,7 +420,7 @@ def wildlife_quiz():
     print("Wildlife Quiz Complete!")
     print("=" * 40)
 
-    print(f"Your score: {score}/{len(questions)}")
+    print(f"🎉 {player_name}, your score is {score}/{len(questions)}!")
 
     percent = (score / len(questions)) * 100
 
@@ -262,7 +440,9 @@ def wildlife_quiz():
         print("📚 Time to learn more about African wildlife!")
 
     print()
-
+    total_questions += len(questions)
+    total_correct += score
+    quizzes_played += 1
     input("Press Enter to return to the main menu...")
 
 # ------------------------------------------------
@@ -276,7 +456,9 @@ def facts_quiz():
     A quiz that teaches users interesting facts
     about Africa.
     """
-
+    global total_questions
+    global total_correct
+    global quizzes_played
     # Each tuple contains:
     # (Question, Correct Answer)
     facts = [
@@ -335,7 +517,7 @@ def facts_quiz():
 
         else:
 
-            print(f"❌ Incorrect! The correct answer is {answer.title()}.")
+            print(f"Incorrect! The correct answer is {answer.title()}.")
             print()
 
     # -----------------------------
@@ -345,7 +527,7 @@ def facts_quiz():
     print("Quiz Finished!")
     print("=" * 45)
 
-    print(f"You scored {score}/{len(facts)}")
+    print(f"🎉 {player_name}, you scored {score}/{len(facts)}!")
 
     percentage = (score / len(facts)) * 100
 
@@ -365,6 +547,9 @@ def facts_quiz():
         print("📚 Explore more about Africa and try again!")
 
     print()
+    total_questions += len(facts)
+    total_correct += score
+    quizzes_played += 1
 
     input("Press Enter to return to the main menu...")
 
